c73fab1b6029499788763fe9b51ff321d114a4b9
Lucas Sousa <113481857+LucasKater@users.noreply.github.com>
2023-01-20 08:47:48 -0300

    Add files via upload

---
+++ dist/js/about.aa1a1337.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkpokedex"]=self["webpackChunkpokedex"]||[]).push([[443],{1272:function(e,n,t){t.r(n),t.d(n,{default:function(){return i}});var u=function(){var e=this;e._self._c;return e._m(0)},s=[function(){var e=this,n=e._self._c;return n("div",{staticClass:"about"},[n("h1",[e._v("This is an about page")])])}],a=t(1001),l={},r=(0,a.Z)(l,u,s,!1,null,null,null),i=r.exports}}]);
+//# sourceMappingURL=about.aa1a1337.js.map
